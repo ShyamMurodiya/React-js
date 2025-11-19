562d45402f20158e22eadaadf5b0d16384454b77
ShyamMurodiya <shyammurodiya@gmail.com>
2025-11-19 15:58:06 +0530

    day 2

---
+++ component/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import card from './componets/card.jsx'
+
+const App = () => {
+  return (
+    <div>
+      Omega🧸
+      <div className='card'> <h1>in progress ↺</h1>
+      <p id='para'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis nam, adipisci ex accusamus corrupti qui.</p></div>
+      {card()}
+    </div>
+  )
+}
+
+export default App
