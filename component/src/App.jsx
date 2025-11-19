b4b659e91f88f9746e0f858ce64c3b277f8be902
ShyamMurodiya <shyammurodiya@gmail.com>
2025-11-19 17:01:41 +0530

    Day 2 evening

--- component/src/App.jsx
+++ component/src/App.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import card from './componets/card.jsx'
+import Card from './componets/card.jsx'
 
 const App = () => {
   return (
     <div>
       Omega🧸
       <div className='card'> <h1>in progress ↺</h1>
-      <p id='para'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis nam, adipisci ex accusamus corrupti qui.</p></div>
-      {card()}
+      <Card/>
+      <Card/>
+      <Card/>
+      </div>
+      <Card/>
+
     </div>
   )
 }
 
 export default App
+ 
--- component/src/componets/card.jsx
+++ component/src/componets/card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const card = () => {
   return (
     <div>
-      
+      <p id='para'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis nam, adipisci ex accusamus corrupti qui.</p>
     </div>
   )
 }
